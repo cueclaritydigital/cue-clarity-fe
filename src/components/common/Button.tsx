eaf979301328c348df2624d61df5cc1a182c1483
Abinash <abinashshasini125@gmail.com>
2026-04-13 21:07:39 +0530

    button add

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,5 +1,5 @@
-// "use client";
+"use client";
 
-// export default function Button({ children, className }) {
-//   <button className="className">{children}</button>;
-// }
+export default function Button({ children, className }) {
+  <button className="className">{children}</button>;
+}
